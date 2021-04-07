ce4a4a2618a8f3d72b42b88bff6a8600e098804c
Volodymyr7711 <volodumurbodnarchuk70@gmail.com>
2021-04-07 15:40:04 +0300

    Initial Commit

---
+++ CovidMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidMVVM
+//
+//  Created by Volodymyr Mendyk on 07.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
